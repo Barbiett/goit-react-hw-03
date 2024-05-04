c30034bce62955f3684d0b6aec1adce67dff85ae
Barbiet <bogdanvinokurov2000@gmail.com>
2024-05-04 18:14:53 +0300

    All done.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,44 @@
-export default function Contact({ handleClick: { id, name, number } }) {
+import { FaUser } from "react-icons/fa6";
+import { FaPhoneAlt } from "react-icons/fa";
+import css from "./Contact.module.css";
+export default function Contact({
+  contact: { name, number, id },
+  deleteContact,
+}) {
   return (
-    <div>
-      <ul>
-        <li>
-          <span>{id}</span>
-          <span>{name}</span>
-          <span>{number}</span>
-        </li>
-        ;
-      </ul>
-    </div>
+    // <li className={css.item}>
+    //   <div className={css.span}>
+    //
+    //     <span>{name}</span>
+    //
+    //     <span>{number}</span>
+    //   </div>
+    //   <button className={css.button} onClick={() => deleteContact(id)}>
+    //     Delete
+    //   </button>
+    // </li>
+    <li style={{ listStyle: "none" }}>
+      <div className={css.listItem}>
+        <div className={css.info}>
+          <p>
+            <FaUser style={{ paddingRight: "20" }} />
+            {name}
+          </p>
+          <p>
+            <FaPhoneAlt style={{ paddingRight: "20" }} />
+            {number}
+          </p>
+          <a href={`tel:+${number}`}></a>
+        </div>
+        <button
+          className={css.button}
+          id={id}
+          type="button"
+          onClick={() => deleteContact(id)}
+        >
+          Delete
+        </button>
+      </div>
+    </li>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,16 @@
-// import Contact from "../Contact/Contact";
-export default function ContactList() {
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+export default function ContactList({ contacts, deleteContact }) {
   return (
-    <div>
-      <ul></ul>
-    </div>
+    <ul className={css.list}>
+      {contacts.map((contact) => (
+        <Contact
+          deleteContact={deleteContact}
+          contact={contact}
+          key={contact.id}
+        />
+      ))}
+    </ul>
   );
 }
